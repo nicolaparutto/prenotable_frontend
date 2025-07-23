20386dc42c2a84e7041783c46543c0889899d9d7
nicolaparutto <nicolaparutto2003@gmail.com>
2025-07-23 18:26:30 +0200

    feat: add first section in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,18 @@
 function HomePage() {
 	return (
-		<div>HomePage</div>
+		<>
+			<section className="py-[60px] text-center flex flex-col items-center md:py-[100px]">
+				<div className="mx-5 mb-2">
+					<h1 className="font-title text-xl color-yellow mb-3">Trova il ristorante che cerchi in pochi click...</h1>
+					<p className="text-sm">Cerca tra migliaia di ristoranti e locali in tutta italia e trova quello che fa per te.</p>
+					<p className="text-sm mt-1">Prenota il tuo tavolo direttamente da qui!</p>
+				</div>
+				<div className="p-3 inset-1 shadow-2xl w-[320px] md:w-[500px]">
+					<input className="w-full border-1 border-yellow rounded-sm p-2 md:w-[70%] md:mr-1" type="text" placeholder="Città, Regione..." />
+					<button className="mt-2 bg-yellow py-[6px] px-[30px] rounded-full text-black cursor-pointer md:py-[8px] md:px-[40px] hover:bg-dark-yellow active:bg-dark-yellow">Cerca</button>
+				</div>
+			</section>
+		</>
 	)
 }
 
