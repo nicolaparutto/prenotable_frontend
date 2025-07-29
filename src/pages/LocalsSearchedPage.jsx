fec2dfa49602bbcfa4bec012d661deff2fac2d09
nicolaparutto <nicolaparutto2003@gmail.com>
2025-07-29 18:37:04 +0200

    fix: queries for receiving data

--- src/pages/LocalsSearchedPage.jsx
+++ src/pages/LocalsSearchedPage.jsx
@@ -1,6 +1,9 @@
 import { useSearchParams } from "react-router-dom"
 import { useEffect } from "react";
 import { useGlobalContext } from "../contexts/GlobalContext";
+
+import LocalCard from "../components/cards/LocalCard";
+
 function LocalsSearchedPage() {
 	const { getSearchedLocals, searchedLocals } = useGlobalContext();
 
@@ -11,14 +14,44 @@ function LocalsSearchedPage() {
 	const params = {
 		where: where
 	};
+	console.log(searchedLocals);
 
 	useEffect(() => {
-		//getSearchedLocals(params)
+		getSearchedLocals(params)
 	}, [where]);
 	return (
-		<div>
-			LocalsSearchedPage
-		</div>
+		<>
+			<div className="text-center mt-1 mb-5">
+				<h1 className="font-title text-xl md:text-xxl mb-3">Risultati di: {where}</h1>
+				<div className="container m-auto px-4 flex justify-between">
+					<h2 className='text-md'>{searchedLocals.length} Locali</h2>
+					<select className="bg-yellow w-[100px] p-1 rounded-sm text-sm">
+						<option value="">Ordina Per</option>
+						<option value="">Prezzo Crescente</option>
+						<option value="">Prezzo Decrescente</option>
+					</select>
+				</div>
+				<div className="bg-light-gray text-left p-4">
+					<h2 className='text-md'>Filtri</h2>
+					<div className="flex justify-between">
+						<select className="bg-yellow p-1 rounded-sm text-sm">
+							<option value="">Tipologia</option>
+						</select>
+						<select className="bg-yellow p-1 rounded-sm text-sm">
+							<option value="">Prezzo</option>
+						</select>
+						<select className="bg-yellow p-1 rounded-sm text-sm">
+							<option value="">Valutazioni</option>
+						</select>
+					</div>
+				</div>
+			</div >
+			<section className="m-auto container">
+				<div>
+					<LocalCard />
+				</div>
+			</section>
+		</>
 	)
 }
 
